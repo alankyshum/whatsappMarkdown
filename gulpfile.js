90fde89f5e69422f5b9efb01de54676addfbac3e
Alan Shum <alan200994@gmail.com>
2016-03-12 23:13:33 +0800

    always enable; MVP

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    cssmin = require('gulp-cssmin'),
+    jsmin = require('gulp-jsmin'),
+    fs = require('fs');
 
 gulp.task('scss', function () {
   return gulp.src(['scss/**/*.scss', 'scss/*.scss'])
@@ -7,7 +10,55 @@ gulp.task('scss', function () {
 	.pipe(gulp.dest('./css'));
 });
 
-
 gulp.task('watch', function () {
   gulp.watch(['scss/**/*.scss', 'scss/*.scss'], ['scss']);
 });
+
+// DISTRIBUTIONS
+// -------------
+gulp.task('minify-js', function () {
+  return gulp.src('dist/*.js')
+  .pipe(jsmin())
+  .pipe(gulp.dest('dist'));
+});
+gulp.task('minify-css', function () {
+  return gulp.src('dist/*.css')
+  .pipe(cssmin())
+  .pipe(gulp.dest('dist'));
+});
+gulp.task('flattenManifestPath', function () {
+  fs.readFile('manifest.json', 'utf8', (err, json) => {
+    json = JSON.parse(json);
+    json.content_scripts.forEach((script) => {
+      script.css = script.css.map((csspath) => {
+        return csspath.split('/').pop()
+      });
+      script.js = script.js.map((jspath) => {
+        return jspath.split('/').pop()
+      })
+    }) // end: foreach
+    fs.writeFile('dist/manifest.json', JSON.stringify(json), 'utf8', (err) => {
+      if (err) console.error(err);
+    });
+  })
+});
+gulp.task('migrate-assets', function () {
+  return gulp.src(['assets/**/**', '!assets/**/*.db'])
+  .pipe(gulp.dest('dist/assets'));
+});
+gulp.task('migrate', ['migrate-assets'], function () {
+  var usedFiles = [
+    'js/**/*.js',
+    'css/**/*.css',
+    "bower_components/prism/themes/prism-okaidia.css",
+    "bower_components/prism/plugins/line-numbers/prism-line-numbers.css",
+    "bower_components/markdown-it/dist/markdown-it.min.js",
+    "bower_components/prism/prism.js",
+    "bower_components/prism/plugins/line-numbers/prism-line-numbers.min.js",
+    'LICENSE'
+  ]
+  return gulp.src(usedFiles)
+  .pipe(gulp.dest('dist'))
+});
+
+gulp.task('dist', ['scss', 'migrate', 'minify-js', 'minify-css', 'flattenManifestPath']);
--- js/client.js
+++ js/client.js
@@ -2,15 +2,31 @@
 	'use strict';
 
 	var md = markdownit();
+	var _TAGS = ["<markdown>", "//md//"],
+		_TAG_HTML = "<span class='markdownLabel'> </span>";
 	var checkMsgBubbles = setInterval(() => {
 		var msgBubble = document.querySelectorAll('.emojitext.selectable-text:not(.markdownParsed)');
-		console.warn(`${msgBubble.length} bubbles with markdown checked`);
 		Object.keys(msgBubble).forEach((i) => {
 			var msgText = msgBubble[i].innerText;
-			if (~msgText.indexOf("<markdown>")) {
-				console.warn(msgText);
-				msgBubble[i].innerHTML = md.render(msgText);
+			var _usedTag = null;
+			_TAGS.every((tag) => {
+				if (~msgText.indexOf(tag)) {
+					_usedTag = tag;
+					return false;
+				}
+				return true;
+			})
+			if (_usedTag) {
+				msgText = msgText.slice(_usedTag.length);
+				msgBubble[i].innerHTML = _TAG_HTML + md.render(msgText);
 				msgBubble[i].classList.add('markdownParsed');
+
+				// SYNTAX HIGHLIGHT
+				var codes = msgBubble[i].querySelectorAll('code[class*=language-]');
+				Object.keys(codes).forEach((code_i) => {
+					codes[code_i].parentNode.classList.add('line-numbers');
+					Prism.highlightElement(codes[code_i]);
+				});
 			}
 		});
 	}, 1000);
